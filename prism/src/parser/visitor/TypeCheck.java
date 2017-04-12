c0d85c8e93ddc19aaecc0d0f5ddf5545c9ea5475
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2017-04-12 13:36:07 +0100

    parse approx operator

--- prism/src/parser/visitor/TypeCheck.java
+++ prism/src/parser/visitor/TypeCheck.java
@@ -425,6 +425,7 @@ public class TypeCheck extends ASTTraverse
 			}
 			break;
 		case ExpressionFunc.PARTIAL:
+		case ExpressionFunc.APPROX:
 			// Anything goes
 			break;
 		default:
@@ -460,6 +461,7 @@ public class TypeCheck extends ASTTraverse
 			break;
 		case ExpressionFunc.MULTI:
 		case ExpressionFunc.PARTIAL:
+		case ExpressionFunc.APPROX:
 			// Resulting type is always same as first arg
 			if (types[0] instanceof TypeBool)
 				e.setType(TypeBool.getInstance());
