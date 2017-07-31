27fa585e9ccbacf1d5c73d4ce9b1559c44cdb7af
fatmaf <ffaruq@gmail.com>
2017-07-31 15:03:42 +0100

    deleted old test folder

--- prism/tests/fatma_tests/old/dotfiles/convertall.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-for file in `ls *.dot`
-do 
-    dot -Tpdf -o ${file/.dot/}.pdf $file
-done
--- prism/tests/fatma_tests/old/test_exec.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-fn=$1
-mext=$2
-prop=$3
-comm="../../bin/prism $fn.$mext $prop.props -ex -exportadv ${fn}_adv.tra -exportprodstates ${fn}_prod.sta -exportstates $fn.sta -exportlabels $fn.lab -exportprodtrans ${fn}_prod.tra -exporttarget ${fn}_tar.lab -exporttransdot ${fn}_trans.dot -exporttransdotstates ${fn}_sta.dot"
-echo $comm
-eval $comm
