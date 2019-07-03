573fac4c4a9aae1ed69016c962a39bd79c364dbf
fatmaf <ffaruq@gmail.com>
2019-07-03 17:37:16 +0100

    added files to generate prism files using a gui, half done, doesnt do anything with avoid states and goal states, will fix that tomorrow

---
+++ prism/tests/wkspace/gridgui.py
@@ -0,0 +1,251 @@
+from tkinter import *
+from GeneratePrismFiles import GeneratePrismFile
+
+MAP="map"
+SETINITLOCS="initlocs"
+SETGOALS="goals"
+SETAVOID="avoid"
+SETFS = "fail"
+xside = 0
+yside = 0
+
+appState = MAP
+gridArray = None
+blockedCells = None
+
+class Cell():
+    FILLED_COLOR_BG = "black"
+    EMPTY_COLOR_BG = "white"
+    FILLED_COLOR_BORDER = "black"
+    EMPTY_COLOR_BORDER = "black"
+    START_COLOR_BG = "blue"
+    START_COLOR_BORDER = "blue"
+    GOAL_COLOR_BG="green"
+    GOAL_COLOR_BORDER = "green"
+    AVOID_COLOR_BG="red"
+    AVOID_COLOR_BORDER="red"
+    FAIL_COLOR_BG="grey"
+    FAIL_COLOR_BORDER="grey"
+
+    def __init__(self, master, x, y, size):
+        """ Constructor of the object called by Cell(...) """
+        self.master = master#Canvas(master)
+        self.abs = x
+        self.ord = y
+        self.size= size
+        self.fill= False
+        self.isInitPos = False
+        self.isGoalPos = False
+        self.isFailState = False
+        self.isAvoidState = False 
+        
+
+    def _switch(self):
+        """ Switch if the cell is filled or not. """
+        global appState 
+        #print (appState)
+        if appState is MAP:
+            self.fill= not self.fill
+        elif appState is SETINITLOCS:
+            self.isInitPos = not self.isInitPos
+        elif appState is SETAVOID:
+            self.isAvoidState = not self.isAvoidState
+        elif appState is SETFS:
+            self.isFailState = not self.isFailState
+        elif appState is SETGOALS:
+            self.isGoalPos = not self.isGoalPos
+        else:
+            print ("invalid app state")
+
+    def draw(self):
+        """ order to the cell to draw its representation on the canvas """
+        if self.master != None :
+            fill = Cell.EMPTY_COLOR_BG
+            outline = Cell.EMPTY_COLOR_BORDER
+            global appState
+            
+            if appState is MAP:
+                if self.fill:
+                    fill = Cell.FILLED_COLOR_BG
+                    outline = Cell.FILLED_COLOR_BORDER
+                    
+            elif appState is SETINITLOCS:
+                if self.isInitPos:
+                    fill = Cell.START_COLOR_BG
+                    outline = Cell.START_COLOR_BORDER
+            elif appState is SETAVOID:
+                if self.isAvoidState:
+                    fill = Cell.AVOID_COLOR_BG
+                    outline = Cell.AVOID_COLOR_BORDER
+
+            elif appState is SETFS:
+                if self.isFailState:
+                    fill = Cell.FAIL_COLOR_BG
+                    outline = Cell.FAIL_COLOR_BORDER
+            elif appState is SETGOALS:
+                if self.isGoalPos:
+                    fill = Cell.GOAL_COLOR_BG
+                    outline = Cell.GOAL_COLOR_BORDER
+                    
+                
+                    
+
+            xmin = self.abs * self.size
+            xmax = xmin + self.size
+            ymin = self.ord * self.size
+            ymax = ymin + self.size
+
+            self.master.create_rectangle(xmin, ymin, xmax, ymax, fill = fill, outline = outline)
+
+class CellGrid(Canvas):
+    def __init__(self,master, rowNumber, columnNumber, cellSize, *args, **kwargs):
+        Canvas.__init__(self, master, width = cellSize * columnNumber , height = cellSize * rowNumber, *args, **kwargs)
+
+        self.cellSize = cellSize
+
+        self.grid = []
+        for row in range(rowNumber):
+
+            line = []
+            for column in range(columnNumber):
+                line.append(Cell(self, column, row, cellSize))
+
+            self.grid.append(line)
+
+        #memorize the cells that have been modified to avoid many switching of state during mouse motion.
+        self.switched = []
+        #self.canvas = Canvas(self)
+
+        #bind click action
+        self.bind("<Button-1>", self.handleMouseClick)  
+        #bind moving while clicking
+        self.bind("<B1-Motion>", self.handleMouseMotion)
+        #bind release button action - clear the memory of midified cells.
+        self.bind("<ButtonRelease-1>", lambda event: self.clearSwitced())
+
+        self.draw()
+
+
+    def clearSwitched():
+        self.switched*=0
+        
+    def draw(self):
+        for row in self.grid:
+            for cell in row:
+                cell.draw()
+
+    def _eventCoords(self, event):
+        row = int(event.y / self.cellSize)
+        column = int(event.x / self.cellSize)
+        return row, column
+
+    def handleMouseClick(self, event):
+        row, column = self._eventCoords(event)
+        cell = self.grid[row][column]
+        cell._switch()
+        cell.draw()
+        #add the cell to the list of cell switched during the click
+        self.switched.append(cell)
+
+    def handleMouseMotion(self, event):
+        row, column = self._eventCoords(event)
+        cell = self.grid[row][column]
+
+        if cell not in self.switched:
+            cell._switch()
+            cell.draw()
+            self.switched.append(cell)
+
+def addInitialLocations():
+    global appState
+    appState = SETINITLOCS
+    #print (appState)
+    
+def addGoals():
+    global appState
+    appState = SETGOALS
+    #print (appState)
+
+def addFailStates():
+    global appState
+    appState = SETFS
+
+def addAvoidStates():
+    global appState
+    appState = SETAVOID
+
+def allDone():
+    #get all the goal states and stuff
+    goalStates = []
+    failStates = []
+    initStates = []
+    blockedStates =[]
+    avoidStates = []
+    connectedStates=[]
+    global gridArray
+    if gridArray is not None:
+        for i in range(len(gridArray)):
+            for j in range(len(gridArray[i])):
+                c = gridArray[i][j]
+                d = (i,j)
+                if c.fill:
+                    blockedStates.append(d)
+                elif c.isInitPos:
+                    initStates.append(d)
+                elif c.isGoalPos:
+                    goalStates.append(d)
+                    if c.isFailState:
+                        failStates.append(d)
+                elif c.isFailState:
+                    failStates.append(d)
+                elif c.isAvoidState:
+                    avoidStates.append(d)
+                if not c.fill:
+                    connectedStates.append(d)
+    print ("Init")
+    print (initStates)
+    print ("Goals")
+    print (goalStates)
+    print ("Fail")
+    print (failStates)
+    print ("Avoid")
+    print (avoidStates)
+    print ("blocked")
+    print (blockedStates)
+    print ("open")
+    print (connectedStates)
+    gfr = GeneratePrismFile()
+    global xside
+    global yside
+    
+    gfr.generateFromGUIGrid(initStates,blockedStates,goalStates,avoidStates,failStates,connectedStates,xside,yside)
+                
+if __name__ == "__main__" :
+    global xside 
+    xside = int(input("Enter x side"))
+    global yside
+    yside = int(input("Enter y side"))
+    app = Tk()
+    menu = Menu(app)
+    menu.add_command(label='Add initial locations',command = addInitialLocations)
+    menu.add_command(label='Add goal locations',command=addGoals)
+    menu.add_command(label='failstates',command=addFailStates)
+    menu.add_command(label='avoid state', command=addAvoidStates)
+    
+    menu.add_command(label='done',command=allDone)
+    
+    app.config(menu=menu)
+    #app2 = Tk()
+    
+    #btn = Button(app2,text="Test")
+    #btn.grid(column=51,row=51)
+    grid = CellGrid(app, xside,yside, 10)
+    global gridArray
+    gridArray = grid.grid
+    
+    grid.pack()
+
+
+    
+    app.mainloop()
+    #app2.mainloop()
