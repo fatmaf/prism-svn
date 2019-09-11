1e48eb6d7bfab580dfb65d4f3bfd3e2a60583a45
fatmaf <ffaruq@gmail.com>
2019-09-11 13:29:52 +1000

    it'll just be easier to work on all this in the same branch so I'm going to merge the two

---
+++ prism/src/demos/MDPCreator.java
@@ -0,0 +1,223 @@
+/**
+ * 
+ */
+package demos;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+
+import explicit.Distribution;
+import explicit.MDPSimple;
+import parser.State;
+import prism.PrismFileLog;
+import prism.PrismLog;
+
+/**
+ * @author fatma
+ * a class that can be used to create an MDP 
+ * why because there's plenty of book keeping to do 
+ * and it can be annoying 
+ * 
+ * it can also be used to mask some MDP things that I use regularly 
+ * cuz mein thak gai hun ye kar kar k 
+ * so reusable code 
+ * I will probably add reward structures to this too 
+ * cuz like bus yaar 
+ * 
+ * hey old me this was a really great move 
+ * i'm proud of you =D 
+ */
+public class MDPCreator
+{
+
+	boolean printMessages;
+	MDPSimple mdp;
+	HashMap<State, Integer> stateIndices;
+
+	PrismLog mainLog;
+
+	public MDPCreator(PrismLog mainLog)
+	{
+		printMessages = false;
+		mdp = new MDPSimple();
+		List<State> statesList = new ArrayList<State>();
+		mdp.setStatesList(statesList);
+		stateIndices = new HashMap<State, Integer>();
+		this.mainLog = mainLog;
+	}
+	public MDPCreator()
+	{
+		printMessages =false;
+		mdp = new MDPSimple();
+		List<State> statesList = new ArrayList<State>();
+		mdp.setStatesList(statesList);
+		stateIndices = new HashMap<State, Integer>();
+		this.mainLog = null;
+	}
+
+	public void setPrintMessagesOn()
+	{
+		this.printMessages = true;
+	}
+
+	public void setPrintMessagesOff()
+	{
+		this.printMessages = false;
+	}
+
+	public boolean addState(State s)
+	{
+		boolean added = false;
+		if (!stateIndices.containsKey(s)) {
+			int stateIndex = mdp.getNumStates();
+			mdp.addState();
+			stateIndices.put(s, stateIndex);
+			mdp.getStatesList().add(s);
+			added = true;
+
+		}
+		return added;
+	}
+
+	int getStateIndex(State s)
+	{
+		if (!stateIndices.containsKey(s)) {
+			boolean added = addState(s);
+			if (added) {
+				if (printMessages) {
+					mainLog.println("State added");
+				}
+			}
+		}
+		return stateIndices.get(s);
+	}
+
+	int getActionIndex(int stateIndex, Object a)
+	{
+		int actionIndex = -1;
+		if (stateIndex != -1) {
+			int choices = mdp.getNumChoices(stateIndex);
+			for (int c = 0; c < choices; c++) {
+				Object act = mdp.getAction(stateIndex, c);
+				if (act.toString().contentEquals(a.toString())) {
+					actionIndex = c;
+					break;
+				}
+			}
+		}
+		return actionIndex;
+	}
+
+	public boolean addAction(State s, Object a, ArrayList<Entry<State, Double>> successorsWithProbs)
+	{
+
+		//add an action to a state
+		//does not check if the same action is added twice!!! 
+		//it shouldnt be you know 
+		Distribution distr = new Distribution();
+		int stateIndex = getStateIndex(s);
+		int actionIndex = mdp.getNumChoices(stateIndex);
+		for (int i = 0; i < successorsWithProbs.size(); i++) {
+			Entry<State, Double> stateProbPair = successorsWithProbs.get(i);
+			State succState = stateProbPair.getKey();
+			double prob = stateProbPair.getValue();
+			int succStateIndex = getStateIndex(succState);
+			distr.add(succStateIndex, prob);
+		}
+		mdp.addActionLabelledChoice(stateIndex, distr, a);
+		return true; 
+	}
+	
+	//adds an action or parts of an action 
+	public boolean addActionPartial(State s, Object a, ArrayList<Entry<State,Double>>successorsWithProbs)
+	{
+	
+		boolean added = false;
+		int stateIndex = getStateIndex(s); 
+		int actionIndex = getActionIndex(stateIndex,a); 
+		if(actionIndex == -1)
+		{
+			added=addAction(s,a,successorsWithProbs);
+		}
+		else
+		{
+			//this action exists 
+			//so we're just going to add to it 
+			Distribution distr = mdp.getChoice(stateIndex, actionIndex); 
+			if(printMessages)
+			{
+				mainLog.println("Action "+a.toString()+" exists, adding to it"); 
+			
+			Iterator<Entry<Integer, Double>> distList = distr.iterator(); 
+			String succStatesString = "Existing states: ";
+			while(distList.hasNext()) {
+				Entry<Integer, Double> stateProbPair = distList.next();
+				State sas = mdp.getStatesList().get(stateProbPair.getKey());
+				succStatesString+=" "+sas.toString()+":"+stateProbPair.toString();
+			}
+			mainLog.println(succStatesString);
+			}
+			for(int i = 0; i<successorsWithProbs.size(); i++)
+			{
+				Entry<State, Double> stateProbPair = successorsWithProbs.get(i); 
+				State state = stateProbPair.getKey(); 
+				int succStateIndex = getStateIndex(state); 
+				double prob = stateProbPair.getValue(); 
+				//if we dont have a transition to this state 
+				//add it 
+				if(distr.contains(succStateIndex))
+				{
+					if(printMessages)
+						mainLog.println(state.toString()+" already exists!! Not adding it."); 
+				}
+				else
+				{
+					mdp.getChoice(stateIndex, actionIndex).add(succStateIndex, prob);
+					if(!added)
+						added=true;
+				}
+				
+			}
+			
+		}
+		return added;
+	}
+	
+	
+	public void setInitialState(State s)
+	{
+		int sIndex = getStateIndex(s); 
+		mdp.addInitialState(sIndex);
+	}
+	
+	public List<State> getInitialStates()
+	{
+		List<State> initStates = new ArrayList<State>(); 
+		Iterable<Integer> iterable = mdp.getInitialStates(); 
+		Iterator<Integer> iter = iterable.iterator(); 
+		while(iter.hasNext())
+		{
+			int s = iter.next(); 
+			State ss = mdp.getStatesList().get(s); 
+			initStates.add(ss);
+		}
+		return initStates; 
+	}
+	
+	public State getFirstInitialState()
+	{
+		int s = mdp.getFirstInitialState(); 
+		return mdp.getStatesList().get(s);
+	}
+	
+	public void saveMDP(String saveLoc, String name)
+	{
+		String fn = saveLoc + name +".dot";
+		PrismLog out = new PrismFileLog(fn);
+		mdp.exportToDotFile(out, null, true);
+		out.close();
+	}
+}
--- prism/src/demos/TempXAI.java
+++ prism/src/demos/TempXAI.java
@@ -3,11 +3,14 @@ package demos;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.BitSet;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.Vector;
+import java.util.AbstractMap;
+import java.util.AbstractMap.SimpleEntry;
 
 import acceptance.AcceptanceOmega;
 import acceptance.AcceptanceType;
@@ -21,6 +24,7 @@ import parser.ast.ASTElement;
 import parser.ast.Expression;
 import parser.ast.ExpressionFunc;
 import parser.ast.ExpressionProb;
+import parser.ast.ExpressionReward;
 import parser.ast.ModulesFile;
 import parser.ast.PropertiesFile;
 import prism.Prism;
@@ -28,7 +32,10 @@ import prism.PrismException;
 import prism.PrismFileLog;
 import prism.PrismLog;
 import prism.PrismSettings;
+import prism.Result;
 import simulator.ModulesFileModelGenerator;
+import strat.MDStrategy;
+import strat.Strategy;
 
 public class TempXAI
 {
@@ -42,7 +49,7 @@ public class TempXAI
 	public void mdpActionsToDAPropsNoProduct() throws PrismException, FileNotFoundException
 	{
 		String saveplace = "/home/fatma/Data/PhD/code/prism_ws/prism-svn/prism/tests/wkspace/simpleTests/";//"/home/fatma/Data/phD/work/code/mdpltl/prism-svn/prism/tests/decomp_tests/";
-		String filename = "g7x3_r2_t3_d0_fs1";//"robot";
+		String filename = "g5_r2_t3_d2_fs1";//"g7x3_r2_t3_d0_fs1";//"robot";
 		// Create a log for PRISM output (hidden or stdout)
 		//PrismLog mainLog = new PrismDevNullLog();
 		PrismLog mainLog = new PrismFileLog("stdout");
@@ -67,8 +74,8 @@ public class TempXAI
 		mc.setModulesFileAndPropertiesFile(modulesFile, propertiesFile, new ModulesFileModelGenerator(modulesFile, prism));
 
 		//get the property for a single model 
-//		for (int i = 0; i < propertiesFile.getNumProperties(); i++)
-//			mainLog.println(propertiesFile.getProperty(i));
+		//		for (int i = 0; i < propertiesFile.getNumProperties(); i++)
+		//			mainLog.println(propertiesFile.getProperty(i));
 		ExpressionProb exprProb = (ExpressionProb) propertiesFile.getProperty(0);
 		Expression expr = exprProb.getExpression();
 
@@ -78,13 +85,13 @@ public class TempXAI
 		Vector<BitSet> labelBS = new Vector<BitSet>();
 		DA<BitSet, ? extends AcceptanceOmega> da = mcLTL.constructDAForLTLFormula(mc, mdp, expr, labelBS, allowedAcceptance);
 		mainLog.println(da.size());
-		
+
 		//initialise to get labels for actions 
 		int numAPs = da.getAPList().size();
 		BitSet s_labels = new BitSet(numAPs);
 		mainLog.println(da.getAPList().toString());
 		mainLog.println(expr.toString());
-		
+
 		//associate a label with each state in the mdp 
 		ArrayList<BitSet> statelabels = new ArrayList<BitSet>();
 		for (int s_0 : new IterableStateSet(mdp.getNumStates())) {
@@ -98,7 +105,7 @@ public class TempXAI
 			mainLog.println(mdp.getStatesList().get(s_0) + " " + s_0 + "," + q_0 + ":" + s_labels.toString());
 			statelabels.add(s_labels);
 		}
-		
+
 		//attach a label to an action of the mdp 
 		//basically that of the next state 
 		HashMap<Object, ArrayList<BitSet>> actionLabels = new HashMap<Object, ArrayList<BitSet>>();
@@ -126,13 +133,13 @@ public class TempXAI
 		}
 		//map da stuff to actions 
 		//so essentially form bins 
-		
+
 	}
-	
+
 	public void mdpActionsToDAPropsProduct() throws PrismException, FileNotFoundException
 	{
 		String saveplace = "/home/fatma/Data/PhD/code/prism_ws/prism-svn/prism/tests/wkspace/simpleTests/";//"/home/fatma/Data/phD/work/code/mdpltl/prism-svn/prism/tests/decomp_tests/";
-		String filename = "g7x3_r2_t3_d0_fs1";//"robot";
+		String filename = "g5_r2_t3_d2_fs1";//"g7x3_r2_t3_d0_fs1";//"robot";
 		// Create a log for PRISM output (hidden or stdout)
 		//PrismLog mainLog = new PrismDevNullLog();
 		PrismLog mainLog = new PrismFileLog("stdout");
@@ -148,7 +155,7 @@ public class TempXAI
 		String modelFileName = saveplace + filename + "0.prism";
 		ModulesFile modulesFile = prism.parseModelFile(new File(modelFileName));
 		prism.loadPRISMModel(modulesFile);
-		PropertiesFile propertiesFile = prism.parsePropertiesFile(modulesFile, new File(saveplace + filename + ".props"));
+		PropertiesFile propertiesFile = prism.parsePropertiesFile(modulesFile, new File(saveplace + filename + "_rew.props"));
 		prism.buildModel();
 		MDP mdp = (MDP) prism.getBuiltModelExplicit();
 
@@ -157,11 +164,30 @@ public class TempXAI
 		mc.setModulesFileAndPropertiesFile(modulesFile, propertiesFile, new ModulesFileModelGenerator(modulesFile, prism));
 
 		//get the property for a single model 
-//		for (int i = 0; i < propertiesFile.getNumProperties(); i++)
-//			mainLog.println(propertiesFile.getProperty(i));
-		ExpressionProb exprProb = (ExpressionProb) propertiesFile.getProperty(0);
-		Expression expr = exprProb.getExpression();
+		//		for (int i = 0; i < propertiesFile.getNumProperties(); i++)
+		//			mainLog.println(propertiesFile.getProperty(i));
+		ExpressionReward exprRew = (ExpressionReward) propertiesFile.getProperty(0);
+		mc.setExportAdv(true);
+		mc.setExportAdvFilename(saveplace + "results/xai_" + filename + ".dot");
+		mc.setExportProductVector(true);
+		mc.setExportProductVectorFilename(saveplace + "results/xai_" + filename + "_res.tra");
+		mc.setGenStrat(true);
+		mc.setExportProductStates(true);
+		mc.setExportProductTrans(true);
+		mc.setExportProductStatesFilename(saveplace + "results/xai_" + filename + ".sta");
+		mc.setExportProductTransFilename(saveplace + "results/xai_" + filename + ".tra");
 
+//		Result result = mc.check(mdp, propertiesFile.getProperty(0));
+//		MDStrategy strat = (MDStrategy) result.getStrategy();
+//		// mc.computeReachProbs(mdp, target, min)
+//		System.out.println(result.getResult());
+		
+		
+		Expression expr = exprRew.getExpression();
+		Strategy strat = mc.checkPartialSatExprReturnStrategy(mdp,expr,exprRew,null);
+//		double probsGoal[] = mc.computeReachProbs(mdp, statesGoal, false).soln;
+//		System.out.println(Arrays.toString(probsGoal));
+		
 		//convert property to da 
 		LTLModelChecker mcLTL = new LTLModelChecker(prism);
 		AcceptanceType[] allowedAcceptance = { AcceptanceType.RABIN, AcceptanceType.REACH };
@@ -175,7 +201,7 @@ public class TempXAI
 		BitSet s_labels = new BitSet(numAPs);
 		mainLog.println(da.getAPList().toString());
 		mainLog.println(expr.toString());
-		
+
 		//associate a label with each state in the mdp 
 		ArrayList<BitSet> statelabels = new ArrayList<BitSet>();
 		for (int s_0 : new IterableStateSet(mdp.getNumStates())) {
@@ -189,42 +215,45 @@ public class TempXAI
 			mainLog.println(mdp.getStatesList().get(s_0) + " " + s_0 + "," + q_0 + ":" + s_labels.toString());
 			statelabels.add(s_labels);
 		}
-		
+
 		//attach a label to an action of the mdp 
 		//basically that of the next state 
-		HashMap<Object, ArrayList<BitSet>> actionLabels = new HashMap<Object, ArrayList<BitSet>>();
+		HashMap<Entry<Integer, Object>, ArrayList<BitSet>> actionLabels = new HashMap<Entry<Integer, Object>, ArrayList<BitSet>>();
 		for (int s_0 : new IterableStateSet(mdp.getNumStates())) {
 			//go over all these states again 
 			//assign labels to actions as those for the next state 
 			//but what about probabilistic ones ? 
 			int numChoices = mdp.getNumChoices(s_0);
+
 			for (int i = 0; i < numChoices; i++) {
 				Object action = mdp.getAction(s_0, i);
-				if (!actionLabels.containsKey(action)) {
-					actionLabels.put(action, new ArrayList<BitSet>());
+				Entry<Integer, Object> saPair = new AbstractMap.SimpleEntry<Integer, Object>(s_0, action);
+				if (!actionLabels.containsKey(saPair)) {
+					actionLabels.put(saPair, new ArrayList<BitSet>());
 				}
 				Iterator<Entry<Integer, Double>> trIter = mdp.getTransitionsIterator(s_0, i);
 				while (trIter.hasNext()) {
 					Entry<Integer, Double> nextSP = trIter.next();
 					int ns = nextSP.getKey();
-					actionLabels.get(action).add(statelabels.get(ns));
+					actionLabels.get(saPair).add(statelabels.get(ns));
 
 				}
 			}
 		}
-		for (Object a : actionLabels.keySet()) {
+		for (Entry<Integer, Object> a : actionLabels.keySet()) {
 			mainLog.println(a.toString() + " " + actionLabels.get(a).toString());
 		}
 		//map da stuff to actions 
 		//so essentially form bins 
-		
+
 	}
+
 	//run function 
 	public void run()
 	{
 
 		try {
-System.out.println("No Product");
+			System.out.println("No Product");
 			mdpActionsToDAPropsNoProduct();
 			System.out.println("No Product");
 			System.out.println("Product");
